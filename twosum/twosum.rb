4cbe76077341b7a66aa31d43d52d3bb7d44de2fa
YongKeeTan <yongkee.t@gmail.com>
2021-10-23 00:24:25 +0800

    twosum in ruby

---
+++ twosum/twosum.rb
@@ -0,0 +1,17 @@
+def twosum(nums, target)
+  # there are always two numbers that add up to the target in the nums array.
+  # return the indices of the array that add up to target
+  # iterate through the array, store the complement, along with the current index into a hash
+  # once the complement is found in the hash, return the current index and the value that is mapped to the current key
+  complement_hash = {}
+  nums.each_with_index do |num, idx|
+    return [complement_hash[num], idx] if complement_hash.key?(num)
+
+    complement = target - num
+    complement_hash[complement] = idx
+  end
+end
+
+pp twosum([2,7,11,15], 9)
+pp twosum([0,4,3,0], 0)
+pp twosum([-3,4,3,90], 0)
