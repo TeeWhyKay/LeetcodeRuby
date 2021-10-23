50f83ece5f4df1679365521984fcf580d35b1923
YongKeeTan <yongkee.t@gmail.com>
2021-10-23 23:45:48 +0800

    reimplemented validanagram in ruby

--- validAnagram/validAnagram.rb
+++ validAnagram/validAnagram.rb
@@ -1,7 +1,31 @@
 # given two input strings determine if two strings are anagrams of each other
-def valid_anagram?(first, second)
+def valid_anagram?(s, t)
   # anagrams will have the same length and frequency of chars
-  false if first.length != second.length
+  return false if s.length != t.length
 
-  #
+  # count frequency of characters in s string
+  # minus away frequency of characters in t string by iterating through characters
+  # a. return false if the frequency value at key of character is zero during key lookup
+  # b. return false if the frequency value is negative
+  # return true if a. or b. doesn't occur
+  char_freq = {}
+  s.split('').each do |char|
+    char_freq.key?(char) ? char_freq[char] += 1 : char_freq[char] = 1
+  end
+
+  t.split('').each do |char|
+    return false if !char_freq.key?(char) || char_freq[char] <= 0
+
+    char_freq[char] -= 1
+  end
+  true
+
+  # alternate shorter way
+  # return false unless s.length == t.length
+  # s.split("").tally == t.split("").tally
 end
+
+puts valid_anagram?('fish', 'hisf')
+puts valid_anagram?('fish', 'hisfa')
+puts valid_anagram?('fish', 'hisl')
+puts valid_anagram?('ab', 'a')
