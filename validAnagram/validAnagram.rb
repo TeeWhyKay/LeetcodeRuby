805ab1c823890e746a820f24789c6fa5650aca95
YongKeeTan <yongkee.t@gmail.com>
2021-10-23 23:28:45 +0800

    created method base case

---
+++ validAnagram/validAnagram.rb
@@ -0,0 +1,7 @@
+# given two input strings determine if two strings are anagrams of each other
+def valid_anagram?(first, second)
+  # anagrams will have the same length and frequency of chars
+  false if first.length != second.length
+
+  #
+end
