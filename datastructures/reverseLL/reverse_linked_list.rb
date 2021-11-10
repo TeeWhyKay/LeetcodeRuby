107bea65e943148418e3eec6da5e7e810aafe761
YongKeeTan <yongkee.t@gmail.com>
2021-11-11 00:52:20 +0800

    iterative reverse LL

--- datastructures/reverseLL/reverse_linked_list.rb
+++ datastructures/reverseLL/reverse_linked_list.rb
@@ -1,14 +1,22 @@
 class ListNode
-    attr_accessor :val, :next
+    attr_accessor :val, :nxt
 
-    def initialize(val = 0, _next = nil)
+    def initialize(val = 0, nxt = nil)
         @val = val
-        @next = _next
+        @next = nxt
     end
 end
 
 # input head node
+# output head node pointing to last node in original linked list
 def reverse_list(head)
-
-
+  prev = nil
+  curr = head
+  until curr.nil?
+    nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  end
+  prev
 end
