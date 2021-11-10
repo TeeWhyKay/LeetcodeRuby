d3544722c7084dd35a8f5c3caf818204bf212108
YongKeeTan <yongkee.t@gmail.com>
2021-11-10 23:56:00 +0800

    initial commit for reverse linked list

---
+++ datastructures/reverseLL/reverse_linked_list.rb
@@ -0,0 +1,14 @@
+class ListNode
+    attr_accessor :val, :next
+
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+
+# input head node
+def reverse_list(head)
+
+
+end
